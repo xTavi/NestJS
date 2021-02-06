5643e8fb0cf6bf894c518f540f075600438f8589
Octavian Constantin <tavitavi62@gmail.com>
2021-02-06 15:43:47 +0200

    Installed typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432, // from docker-compose file
+      username: 'postgres', // this is the default
+      password: 'pass123', // from docker
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true, // disable in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
