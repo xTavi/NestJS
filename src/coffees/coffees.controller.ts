13c67165d86c8708cf8ac242e5a9200f868b956e
Octavian Constantin <octavian.constantin@bearingpoint.com>
2021-02-06 11:58:51 +0200

    Added new endpoints

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
   findAll() {
     return 'This action returns all the coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return 'This will get the nr ' + id + ' coffee ';
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
   }
 }
