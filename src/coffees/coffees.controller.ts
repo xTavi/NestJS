de945ab6b419d72e23345838b571e8bbed59e1c3
Octavian Constantin <tavitavi62@gmail.com>
2021-02-06 14:49:27 +0200

    Created dto s and filtering requests

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -17,13 +19,13 @@ export class CoffeesController {
   }
 
   @Post()
-  create(@Body() body) {
-    this.coffeesService.create(body);
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesService.create(createCoffeeDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    this.coffeesService.update(id, body);
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
